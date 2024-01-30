d2cd073a8310443f1fe128001e72edfca3b0ff66
Mark Macheta <bawat@hotmail.co.uk>
2024-01-30 06:17:34 +0000

    Inital commit

---
+++ Projector Controls/DisableProjector.bat
@@ -0,0 +1,5 @@
+MultiMonitorTool.exe /disable 3
+nircmd setdefaultsounddevice "Realtek Digital Output" 1
+cd "SendIR/trydrvvb1_1_0/"
+uutx.exe -fProjectorCodes.txt PowerButton
+uutx.exe -fProjectorCodes.txt PowerButton
---
+++ Projector Controls/EnableProjector.bat
@@ -0,0 +1,14 @@
+cd "SendIR/trydrvvb1_1_0/"
+uutx.exe -fProjectorCodes.txt PowerButton
+cd "../.."
+TIMEOUT /T 13
+MultiMonitorTool.exe /enable 3
+cd "SendIR/trydrvvb1_1_0/"
+uutx.exe -fProjectorCodes.txt DownButton
+uutx.exe -fProjectorCodes.txt SelectButton
+uutx.exe -fProjectorCodes.txt DownButton
+uutx.exe -fProjectorCodes.txt RightButton
+uutx.exe -fProjectorCodes.txt SelectButton
+cd "../.."
+nircmd setdefaultsounddevice "Transmitter" 1
+:: nircmd setdefaultsounddevice "MStar Demo" 1
